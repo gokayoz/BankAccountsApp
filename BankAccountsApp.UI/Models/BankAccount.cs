df8b03b4d494dfd497fef54cb27893f2f2e27ff8
Gökay Öz <gokay.oz@outlook.com.tr>
2025-03-02 02:06:33 +0300

    Adds create event

---
+++ BankAccountsApp.UI/Models/BankAccount.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankAccountsApp.UI.Models
+{
+    public class BankAccount
+    {
+        public BankAccount(string owner)
+        {
+            Owner = owner;
+            AccountNumber = Guid.NewGuid();
+            Balance = 0;
+        }
+
+        public string Owner { get; set; }
+        public Guid AccountNumber { get; set; }
+        public decimal Balance { get; private set; }
+
+    }
+}
